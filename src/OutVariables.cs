aa8d8cca3f82cbefe5c15c95912d9ca58cf34ac6
David Pine <IEvangelist@users.noreply.github.com>
2018-03-16 21:35:06 -0500

    Create OutVariables.cs

---
+++ src/OutVariables.cs
@@ -0,0 +1,35 @@
+int LegacyTryPattern(string number) {
+    int i;
+    if (int.TryParse(number, out i)) {
+        return i;
+    }
+    else{ 
+        return -1;
+    }
+
+    // The "i" variable is valid here
+}
+
+int NewTryPattern(string number) {
+    // Note: we can be explicit, but implicit is valid
+    if (int.TryParse(number, out int i)) {
+        return i;
+    }
+    else {
+        return -1; // "i" is available to us here.
+    }
+    // The "i" variables leaks into this scope.
+}
+
+void ScopeExample() {
+    if (DateTime.TryParse("OMG", out var date)) {
+        if (TimeSpan.TryParse("What's happening?", out var ts)) {
+            var newDate = date.Add(ts);
+        }
+    }
+
+    // Note: the "ts" variable is NOT leaked in this scope!
+}
+
+internal int ExpressiveTryPattern(string number)
+    => int.TryParse(number, out int value) ? value : -1;
