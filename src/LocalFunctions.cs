a04c840a72fde08ef0c0c2b34c67fabf027fa3dd
David Pine <IEvangelist@users.noreply.github.com>
2018-03-16 20:33:50 -0500

    Update LocalFunctions.cs

--- src/LocalFunctions.cs
+++ src/LocalFunctions.cs
@@ -1,50 +1,42 @@
-internal static IEnumerable<char> AlphabetSubset(char start, char end)
-{
-    if ((start < 'a') || (start > 'z'))
-    {
-        throw new ArgumentOutOfRangeException(nameof(start),
-                                              "start must be a letter");
+internal static IEnumerable<char> AlphabetSubset(char start, char end) {
+    if ((start < 'a') || (start > 'z')) {
+        throw new ArgumentOutOfRangeException(
+            nameof(start),
+            "start must be a letter");
     }
 
     for (var @char = start; @char < end; ++ @char)
-    {
         yield return @char;
     }
 }
 
-internal static IEnumerable<char> AlphabetSubset2(char start, char end)
-{
-    if ((start < 'a') || (start > 'z'))
-    {
-        throw new ArgumentOutOfRangeException(nameof(start),
-                                              "start must be a letter");
+internal static IEnumerable<char> AlphabetSubset2(char start, char end) {
+    if ((start < 'a') || (start > 'z')) {
+        throw new ArgumentOutOfRangeException(
+            nameof(start),
+            "start must be a letter");
     }
 
     return AlphabetSubsetImpl(start, end);
 }
 
-private static IEnumerable<char> AlphabetSubsetImpl(char start, char end)
-{
-    for (var @char = start; @char < end; ++ @char)
-    {
+private static IEnumerable<char> AlphabetSubsetImpl(char start, char end) {
+    for (var @char = start; @char < end; ++ @char) {
         yield return @char;
     }
 }
 
-internal static IEnumerable<char> AlphabetSubset3(char start, char end)
-{
-    if ((start < 'a') || (start > 'z'))
-    {
-        throw new ArgumentOutOfRangeException(nameof(start), 
-                                              "start must be a letter");
+internal static IEnumerable<char> AlphabetSubset3(char start, char end) {
+    if ((start < 'a') || (start > 'z')) {
+        throw new ArgumentOutOfRangeException(
+            nameof(start), 
+            "start must be a letter");
     }
 
     return alphabetSubsetImpl();
 
-    IEnumerable<char> alphabetSubsetImpl()
-    {
-        for (var @char = start; @char < end; ++ @char)
-        {
+    IEnumerable<char> alphabetSubsetImpl() {
+        for (var @char = start; @char < end; ++ @char) {
             yield return @char;
         }
     }
