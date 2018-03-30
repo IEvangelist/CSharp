8d29dedf7ae0b9914382ccb9077c148839e4167c
David Pine <IEvangelist@users.noreply.github.com>
2018-03-29 21:26:39 -0500

    Update DefaultInterfaceMethods.cs

--- src/DefaultInterfaceMethods.cs
+++ src/DefaultInterfaceMethods.cs
@@ -5,6 +5,17 @@ public interface IDataRepository<T> where T : IRecord {
 public interface IDataRepository<T> where T : IRecord {
     void Create(T value);
     
+    // We want to add this API
+    // But it will break everyone who
+    // doesn't implement it...
+    void CreateAll(T[] values);
+}
+
+public interface IDataRepository<T> where T : IRecord {
+    void Create(T value);
+    
+    // We can provide a default impl.
+    // And allow opt-in, non-breaking!
     void CreateAll(T[] values) {
       foreach (var value in values) {
         Create(value);
