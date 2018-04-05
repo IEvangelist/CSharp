2f05560276fc73a99b125be515a6830d7620c484
David Pine <IEvangelist@users.noreply.github.com>
2018-04-04 20:43:46 -0500

    Update NullableReferenceTypes.cs

--- src/NullableReferenceTypes.cs
+++ src/NullableReferenceTypes.cs
@@ -22,3 +22,35 @@ public void NullReferenceException() {
   // Will throw NullReferenceException!
   Console.WriteLine(person.MiddleName);
 }
+
+public class NewPerson {
+  public string FirstName { get; }
+  public string? MiddleName { get; }
+  public string LastName { get; }
+  
+  // Same .ctor's
+}
+
+public void Warnings() {  
+  var person = new NewPerson("David", "Pine");
+  
+  // Visual Studio Issues a Warning!
+  Console.WriteLine(person.MiddleName);
+}
+
+public void TakeSuggestion() {  
+  var person = new NewPerson("David", "Pine");
+  
+  // Visual Studio Issues a Warning!
+  if (person.MiddleName is null)
+  {
+     return;
+  }
+  
+  Console.WriteLine(person.MiddleName);
+}
+
+
+
+
+// Hacker!
